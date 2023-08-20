4d0af8073618dd7fa74b630ee4676c9a6825c79e
Juan Veronez <juan.monteirov@gmail.com>
2023-08-20 13:30:38 +0000

    Creates header component and styles

---
+++ fe/src/components/Header/styles.ts
@@ -0,0 +1,38 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.header`
+  background: #D73035;
+  height: 12.375rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 76rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .page-details {
+    display: flex;
+    flex-direction: column;
+    gap: 0.375rem;
+
+
+    h1 {
+      color: #fff;
+      font-size: 2rem;
+    }
+
+    h2 {
+      color: #fff;
+      font-size: 1rem;
+      font-weight: 400;
+      opacity: 0.9;
+    }
+  }
+`;
