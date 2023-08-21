c1377ae14efa9e6a4517a120606d60d5c4577332
Juan Veronez <juan.monteirov@gmail.com>
2023-08-21 14:16:40 +0000

    Create components

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,21 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text } from './src/components/Text';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!isFontsLoaded) return null; // not render if fonts not rendereds;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text size={24} >Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="dark" />
     </View>
   );
 }
