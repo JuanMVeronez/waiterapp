897b671b777ae64a57eb8cb289fd8a9d5212fcee
Juan Veronez <juan.monteirov@gmail.com>
2023-08-22 22:10:41 +0000

    finish modal

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
+import { TableModal } from '../components/TableModal';
 
 import {
   CategoriesContainer,
@@ -12,6 +14,13 @@ import {
 } from './styles';
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState<string | null>(null);
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -28,9 +37,17 @@ export function Main() {
       </Container>
       <Footer>
         <SafeFooter>
-          <Button onPress={() => undefined}>Novo Pedido</Button>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>Novo Pedido</Button>
+          )}
         </SafeFooter>
       </Footer>
+
+      <TableModal
+        onSave={handleSaveTable}
+        onClose={() => setIsTableModalVisible(false)}
+        visible={isTableModalVisible}
+      />
     </>
   );
 }
